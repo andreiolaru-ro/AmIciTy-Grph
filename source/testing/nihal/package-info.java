fa380b71e67c6d98e7f446804e589ee85df84dd5
andreiolaru <cs@andreiolaru.ro>
2013-03-13 16:00:57 +0200

    Set up a simple structure.

---
+++ source/testing/nihal/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Testing package for Nihal.
+ */
+package testing.nihal;
+
---
+++ source/testing/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains everything that is not used by the project. Testing files, other code, etc.
+ */
+package testing;
+
