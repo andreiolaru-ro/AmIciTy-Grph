9045679e8a07ba8cde17e018f3aab1a368a6b513
Cătălin Badea <catalin.badea392@gmail.com>
2014-08-19 13:18:55 +0300

    EditorMenu.

---
+++ AmIciTy-Grph-PC/source/amicity/graph/pc/EditorMenu.java
@@ -0,0 +1,15 @@
+package amicity.graph.pc;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+public class EditorMenu extends JMenuBar {
+	private static final long serialVersionUID = -7316178038249679850L;
+	
+	public EditorMenu() {
+		JMenu menu = new JMenu("File");
+		add(menu);
+		menu = new JMenu("Edit");
+		add(menu);
+	}
+}
