21a089c23d52270cb28250b9ad79ddd8fa94fdb4
Cătălin Badea <catalin.badea392@gmail.com>
2014-08-28 15:36:04 +0300

    add laf skins.

--- AmIciTy-Grph-PC/source/amicity/graph/pc/Main.java
+++ AmIciTy-Grph-PC/source/amicity/graph/pc/Main.java
@@ -4,16 +4,46 @@ package amicity.graph.pc;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import amicity.graph.pc.gui.EditorMenu;
 import amicity.graph.pc.gui.Gui;
 
-
+import org.pushingpixels.substance.api.skin.SubstanceBusinessLookAndFeel;
 
 public class Main {
+    public static void useSubstanceLAF() {
+        try {
+
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            UIManager.setLookAndFeel(new SubstanceBusinessLookAndFeel());
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+
+        try {
+
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            //UIManager.setLookAndFeel(new SubstanceBusinessLookAndFeel());
+            //UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceBusinessBlueSteelLookAndFeel");
+            //UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceBusinessBlackSteelLookAndFeel");
+            UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceNebulaBrickWallLookAndFeel");
+            //UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceMistSilverLookAndFeel");
+            //UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGeminiLookAndFeel");
+            //UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGraphiteGlassLookAndFeel");
+        
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
 	
 	public static void buildGUI(MainController controller) {
+		 useSubstanceLAF();
 		JFrame frame = new JFrame("Graph Editor");
+		
 		frame.setContentPane(new Gui(controller));
 		
 		frame.setSize(1000, 800);
